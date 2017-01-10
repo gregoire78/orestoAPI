5c33fbd7749c5f005a34db6952d2d7b31c1866ee
Grégoire <greg.joncour@gmail.com>
2017-01-10 13:38:59 +0100

    ff

--- application/controllers/Restaurant.php
+++ application/controllers/Restaurant.php
@@ -36,6 +36,22 @@ class Restaurant extends MY_Controller {
     }
 
     public function get_restaurant($id){
-        $this->output->set_status_header(200)->set_content_type('text/plain', 'utf-8')->set_output($this->getJson(["restaurant"=>$this->restaurants->get($id)]))->get_output();
+        $data["restaurant"] = [];
+        $restaurant = $this->restaurants->get($id);
+        array_push($data["restaurant"],[
+            "id" => $restaurant->id,
+            "name" => $restaurant->name,
+            "location" => [
+                "city" => $restaurant->city,
+                "address" => $restaurant->address,
+                "postal_code" => $restaurant->postal_code,
+                "longitude" => $restaurant->longitude,
+                "latitude" => $restaurant->latitude
+            ],
+            "description" => $restaurant->description,
+            "image" => $restaurant->image,
+            "dates" => $restaurant->date_register
+        ]);
+        $this->getJson($data);
     }
 }
