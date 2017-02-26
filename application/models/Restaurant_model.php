ceeece84e2eeaac9463f8bfe490c65a02c8e22f8
Grégoire <greg.joncour@gmail.com>
2017-02-26 16:40:31 +0100

    php7 update + optimisations

--- application/models/Restaurant_model.php
+++ application/models/Restaurant_model.php
@@ -4,14 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Restaurant_model extends CI_Model
 {
 
-    private $name;
-    private $city;
-    private $postal_code;
-    private $latitude;
-    private $longitude;
-    private $address;
-    private $description;
-    private $image;
     private $total;
     private $numberPage;
     private $messagesParPage;
@@ -24,37 +16,37 @@ class Restaurant_model extends CI_Model
         $this->numberPage = ceil($this->total / $this->messagesParPage);
     }
 
-    public function getTotalPage()
+    public function getTotalPage(): int
     {
         return $this->numberPage;
     }
 
-    public function getAll()
+    public function getAll(): array
     {
         $query = $this->db->get('restaurants');
         return $query->result();
     }
 
-    public function get($id)
+    public function get(int $id): array
     {
         $query = $this->db->get_where('restaurants', array('id' => $id));
         return $query->result();
     }
 
-    public function get_limit($min, $max)
+    public function get_limit(int $min, int $max): array
     {
         $query = $this->db->select('*')->from('restaurants')->order_by('name')->limit($min, $max)->get();
         return $query->result();
     }
 
-    public function getPage($page)
+    public function getPage(int $page): array
     {
         $pageActuelle = $page;
         $premiereEntree = ($pageActuelle - 1) * $this->messagesParPage; // On calcul la première entrée à lire
         return $this->get_limit($this->messagesParPage, $premiereEntree);
     }
 
-    public function create()
+    public function create(): array
     {
         if (empty($_POST['name']) || empty($_POST['city']) || empty($_POST['postal_code']) || empty($_POST['address']) || empty($_POST['description'])) {
             return ["error" => "il manque quelque chose"];
@@ -63,35 +55,40 @@ class Restaurant_model extends CI_Model
                 'name' => $_POST['name'],
                 'city' => $_POST['city'],
                 'postal_code' => $_POST['postal_code'],
-                'latitude' => empty($_POST['latitude']) ? null : $_POST['latitude'],
-                'longitude' => empty($_POST['longitude']) ? null : $_POST['longitude'],
+                'latitude' => $_POST['latitude'] ?? null,
+                'longitude' => $_POST['longitude'] ?? null,
                 'address' => $_POST['address'],
                 'description' => $_POST['description'],
-                'image' => empty($_POST['image']) ? null : $_POST['image']
+                'image' => $_POST['image'] ?? null
             ];
             $this->db->insert('restaurants', $data);
             return ["success" => "ajout OK"];
         }
     }
 
-    public function update($id)
+    public function update(int $id): array
     {
         $errors = [];
         $warnings = [];
         $result=[];
         $data = array_filter([
-            'name' => empty($_POST['name']) ? null : $_POST['name'],
-            'city' => empty($_POST['city']) ? null : $_POST['city'],
-            'postal_code' => empty($_POST['postal_code']) ? null : $_POST['postal_code'],
-            'latitude' => empty($_POST['latitude']) ? null : $_POST['latitude'],
-            'longitude' => empty($_POST['longitude']) ? null : $_POST['longitude'],
-            'address' => empty($_POST['address']) ? null : $_POST['address'],
-            'description' => empty($_POST['description']) ? null : $_POST['description'],
-            'image' => empty($_POST['image']) ? null : $_POST['image']
+            'name' => $_POST['name'] ?? null,
+            'city' => $_POST['city'] ?? null,
+            'postal_code' => $_POST['postal_code'] ?? null,
+            'latitude' => $_POST['latitude'] ?? null,
+            'longitude' => $_POST['longitude'] ?? null,
+            'address' => $_POST['address'] ?? null,
+            'description' => $_POST['description'] ?? null,
+            'image' => $_POST['image'] ?? null
         ]);
         if (empty($data)) array_push($errors, "Il n'y a rien à mettre à jour !"); //pas de parametres valides
-        elseif ($data["postal_code"] && !preg_match('/^\d{5}$/', $data["postal_code"])) array_push($errors, ["postal_code"=>"le code postal doit être composé de 5 chiffres"]);
-        if (!empty(array_diff_key($_POST, $data))) array_push($warnings,"Paramètres inconnus détectés !", ["unknown_parameters" => array_diff_key($_POST, $data)]); //paramètres inconnues
+        elseif (isset($data["postal_code"]) && !preg_match('/^\d{5}$/', $data["postal_code"])) {
+            $errors["postal_code"] = "le code postal doit être composé de 5 chiffres";
+        }
+        if (!empty(array_diff_key($_POST, $data))) {
+            $warnings["unknown_parameters"] = array_diff_key($_POST, $data);
+            $warnings["message"] = "Paramètres inconnus détectés !";
+        }
 
         if (!empty($errors)) {
             $result["errors"]=$errors;
